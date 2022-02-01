d82583a78e4cb014dd845251599aa403b3849741
aanchalcoder18 <aasaxena14@gmail.com>
2022-02-01 12:08:03 -0800

    figured out layout issues

--- project/src/components/Card.js
+++ project/src/components/Card.js
@@ -3,15 +3,24 @@ import React from "react";
 function Card(props){
     return(
         <div>
-            <div>
-            <img 
-                src={`../images/${props.imageURL}`} 
-            />
-            <p>{props.location}</p>
-            {/* <script src={props.googleMapsUrl}> </script> need to fix */}
-            <h2>{props.title}</h2>
-            <h5>{props.startDate} - {props.endDate}</h5>
-            <p>{props.description}</p>
+            <div className="card">
+                <img 
+                    src={`../images/${props.imageURL}`}
+                    className="card-image" 
+                />
+                <div className="card-overview">
+                    <div className="card-title">
+                        <p>{props.location}</p>
+                        <p className="card-url">View On Google Maps</p>
+                        {/* <script src={props.googleMapsUrl}> </script> need to fix */}
+                    </div>
+                    <h2>{props.title}</h2>
+                    <div className="card-description">
+                        <h5>{props.startDate} - {props.endDate}</h5>
+                        <p>{props.description}</p>
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
